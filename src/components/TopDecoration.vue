10ab365f1a6bcc5b484ef462e60d9d5d6ee42d9f
Tian0820 <151250121@smail.nju.edu.cn>
2018-05-11 14:17:29 +0800

    init

---
+++ src/components/TopDecoration.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <img class="top-decoration1" src="../assets/img/gift1.png"/>
+
+</template>
+
+<script>
+  import Component from 'vue-class-component'
+  import Vue from 'vue';
+
+  export default class TopDecoration extends Vue {
+
+
+  }
+
+</script>
+
+<style scoped src="./TopDecoration.css"></style>
