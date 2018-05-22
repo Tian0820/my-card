457af7c8e0f2f4df2bcb5fbe417acbbf3c5d32bf
Tian0820 <151250121@smail.nju.edu.cn>
2018-05-22 17:07:09 +0800

    <fix>:<change the "strict" parameter to "false" in tsconfig.json / fix import typescript file>

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 // import * as actions from './actions'
 // import * as getters from './getters'
-import auth from './modules/auth.ts'
+import auth from './modules/auth'
 import createLogger from 'vuex/dist/logger'
 
 Vue.use(Vuex)
